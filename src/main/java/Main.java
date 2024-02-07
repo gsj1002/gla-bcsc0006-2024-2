688cf875ad29346e768f0ff01be34def3976cdf3
gsj1002 <singhygovind13@gmail.com>
2024-02-07 22:36:14 +0530

    solution of singly linked list

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,20 @@
+import list.singly.IntegerArrayLinkedList;
+
 public class Main {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+		integerLinkedList list =new integerLinkedList();
+		list.add(24);
+		list.add(25);
+		list.add(26);
+		list.add(27);
+		list.remove(26);
+		System.out.println(list.getSize());
+        list.print();
+
+    }
+
+
+
 	
 	}
-}
+
